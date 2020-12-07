cf956c3f683dc7b1c2bd8b1b1d3a27d7943c6a2e
Drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2020-12-08 00:36:10 +0100

    I hate recursion

--- src/main/java/day7/BagSpec.java
+++ src/main/java/day7/BagSpec.java
@@ -4,18 +4,18 @@ import util.Util;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BagSpec {
     public static String[] input;
     public final String type;
     public final Map<String, Integer> typeAmountContainer;
+    public Set<String> containedIn = new HashSet<>();
 
 
     public BagSpec(String bagSpec) {
-        this.type = bagSpec.split(" contain ")[0];
+        this.type = bagSpec.split(" contain ")[0].replace("bags", "bag");
         String[] containings = bagSpec.split(" contain ")[1].split(", ");
         Map<String, Integer> tempMap = new HashMap<>();
         for (String bag : bagSpec.replace(".", "").split(" contain ")[1].split(", ")) {
@@ -26,7 +26,7 @@ public class BagSpec {
                 tempMap.put(bag.replaceAll("no ", ""), 0);
                 continue;
             }
-            String type = bag.replaceAll("[0-9]+ ", "");
+            String type = bag.replaceAll("[0-9]+ ", "").replace("bags", "bag");
             tempMap.put(type, amount);
         }
         typeAmountContainer = tempMap;
@@ -40,13 +40,26 @@ public class BagSpec {
         }
     }
 
-    public static int ex1() {
-        BagSpec[] bagSpecs = Arrays.stream(input).map(BagSpec::new).toArray(BagSpec[]::new);
-        return 1;
+    public static Set<BagSpec> contructBagSet() {
+        Set<BagSpec> bagSpecs = Arrays.stream(input).map(BagSpec::new).collect(Collectors.toSet());
+        for (BagSpec bagSpec : bagSpecs) {
+            for (String contained : bagSpec.typeAmountContainer.keySet()) {
+                bagSpecs.stream().filter(spec -> spec.type.equals(contained)).forEach(s -> s.containedIn.add(bagSpec.type));
+            }
+        }
+        return bagSpecs;
+    }
+
+    public static Set<BagSpec> findAllContainers(Set<BagSpec> allBagSpecs, String bagSpecName) {
+        return allBagSpecs.stream().filter(spec -> spec.typeAmountContainer.containsKey(bagSpecName)).map(s -> s.containedIn.isEmpty() ? new HashSet<>() : findAllContainers(allBagSpecs, s.type)).reduce((x, y) -> {
+            x.addAll(y);
+            return x;
+        }).get();
     }
 
     public static void main(String[] args) {
-        ex1();
+        Set<BagSpec> bagSpecs = contructBagSet();
+        System.out.println(findAllContainers(bagSpecs, "shiny gold bag").size());
     }
 
 }
