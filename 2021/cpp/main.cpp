75647efbd89a5f366726a33088eb6ce05d23bc2d
Dré van Oorschot <dre@dre-laptop.dre-laptop>
2022-11-09 12:49:06 +0100

    Remove custom exception in favor of proper exit code

--- 2021/cpp/main.cpp
+++ 2021/cpp/main.cpp
@@ -6,7 +6,6 @@
 #include "string"
 
 #include "days/day13.cpp"
-#include "util/exceptions.cpp"
 
 const char* EXAMPLE_PATH = "../inputs/example/%d.txt";
 const char* PUZZLE_PATH = "../inputs/puzzle/%d.txt";
@@ -18,7 +17,7 @@ int main(int argc, char *argv[]) {
     }
     int dayNumber = std::stoi(argv[1]);
     bool exampleMode = argc >= 3 && std::strcmp(argv[2], "-e") == 0;
-    printf("running day %d in %s mode...\n", dayNumber, exampleMode ? "example" : "puzzle");
+    printf("[INFO] running day %d in %s mode...\n", dayNumber, exampleMode ? "example" : "puzzle");
 
     // prep input data
     char path[exampleMode ? strlen(EXAMPLE_PATH) : strlen(PUZZLE_PATH)];
@@ -33,10 +32,11 @@ int main(int argc, char *argv[]) {
         case 13: {
             std::string str = std::string(input);
             Day13(str).run();
-            break;
+            return EXIT_SUCCESS;
         }
         default: {
-            throw UnsupportedDayException("Day "+ std::to_string(dayNumber) +" not supported");
+            std::cerr << "[ERROR] Day " << dayNumber << " not supported";
+            return EXIT_FAILURE;
         }
     }
 }
