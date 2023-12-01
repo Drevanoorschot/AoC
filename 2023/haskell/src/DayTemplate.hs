789ced73e23b33a116ac9fd4d1210a56af9d1d43
Dré van Oorschot <dre.van.oorschot@technolution.nl>
2023-12-01 17:54:06 +0100

    Update template

--- 2023/haskell/src/DayTemplate.hs
+++ 2023/haskell/src/DayTemplate.hs
@@ -1,10 +1,13 @@
 module DayTemplate (solve) where
 
-solve :: String -> (Int, Int)
+import Data.Text hiding (drop, foldl, head, length, reverse, take)
+import Util
+
+solve :: String -> (Answer, Answer)
 solve input = (q1 input, q2 input)
 
-q1 :: String -> Int
-q1 input = 42
+q1 :: String -> Answer
+q1 input = IntAnswer 42
 
-q2 :: String -> Int
-q2 input = 42
+q2 :: String -> Answer
+q2 input = IntAnswer 42
